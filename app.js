de8be2435ab1a9950396cd649395350d7fd77ea2
Emre Samurlu <emrsmrl@gmail.com>
2021-10-11 22:14:45 +0300

    Smart Edu v0.2

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // Third Party Modules
 const express = require("express");
+const mongoose = require("mongoose");
+// My Modules
+const pageRoute = require("./routes/pageRoutes");
+const courseRoute = require("./routes/courseRoute");
 
 // Core Module
 
+//
 const app = express();
+
+// Connect DB
+mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
+  console.log("DB Connected!");
+});
+
 // Template Engine
 app.set("view engine", "ejs");
 
@@ -11,32 +22,8 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-  res.status(200).render("index", {
-    page_name: "index",
-  });
-});
-app.get("/about", (req, res) => {
-  res.status(200).render("about", {
-    page_name: "about",
-  });
-});
-app.get("/courses", (req, res) => {
-  res.status(200).render("courses", {
-    page_name: "courses",
-  });
-});
-app.get("/dashboard", (req, res) => {
-  res.status(200).render("dashboard", {
-    page_name: "dashboard",
-  });
-});
-app.get("/contact", (req, res) => {
-  res.status(200).render("contact", {
-    page_name: "contact",
-  });
-});
-
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 // Server Port
 const port = 3000;
